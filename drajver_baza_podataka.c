90b3d176397745e7da7af729cc147012727d9f74
Kosana66 <kosanapavlovic66@gmail.com>
2022-12-25 17:34:39 +0100

    Uspesno implementirano blokiranje i zastita deljenih resursa + sredjivanje koda da bude pregledan

--- drajver_baza_podataka.c
+++ drajver_baza_podataka.c
@@ -13,18 +13,17 @@
 #include <linux/wait.h>
 #include <linux/semaphore.h>
 #define BAZA_SIZE 10
-#define BUFF_SIZE 50
+#define BUFF_SIZE 70
 MODULE_LICENSE("Dual BSD/GPL");
 
 dev_t my_dev_id;
 static struct class *my_class;
 static struct device *my_device;
 static struct cdev *my_cdev;
-/*
-DECLARE_WAIT_QUEUE_HEAD(readQ);
-DECLARE_WAIT_QUEUE_HEAD(writeQ);
+
+DECLARE_WAIT_QUEUE_HEAD(brisanjeQ);
+DECLARE_WAIT_QUEUE_HEAD(upisQ);
 struct semaphore sem;
-*/
 
 char ime[BAZA_SIZE][100];
 char prezime[BAZA_SIZE][100];
@@ -81,19 +80,22 @@ ssize_t baza_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 
 	if(pos > 0)
 	{
+		//zastita deljenih resursa
+		if(down_interruptible(&sem))
+			return -ERESTARTSYS;
+
+		//sortitranje
 		for(i=0; i<pos-1; i++)
 		{
 			for(j=i+1; j<pos; j++)
 			{
 				if(brBodova[j]<brBodova[i])
 				{							
-					
 					strcpy(tmp_ime,ime[i]);
 					strcpy(tmp_prezime,prezime[i]);		
 					strcpy(tmp_brIndexa,brIndexa[i]);
 					tmp_brBodova=brBodova[i];
-					
-					
+									
 					strcpy(ime[i],ime[j]);
 					strcpy(prezime[i],prezime[j]);		
 					strcpy(brIndexa[i],brIndexa[j]);
@@ -106,7 +108,11 @@ ssize_t baza_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 				}
 			}
 		}
+		
+		//drugi deo zastite resursa
+		up(&sem);
 
+		//citanje baze
 		if(counter<pos)	
 		{	
 			len = scnprintf(buff, BUFF_SIZE, "%s %s %s - %d\n", brIndexa[counter], ime[counter], prezime[counter], brBodova[counter]);
@@ -117,15 +123,12 @@ ssize_t baza_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 		}
 		if(counter==pos)
 		{
-			printk(KERN_INFO "Succesfully read\n");
+			printk(KERN_INFO "Uspesno procitana baza \n");
 			endRead = 1;
 		}
 	}
 	else
-	{
 		printk(KERN_WARNING "Baza je prazna\n"); 
-	}
-
 
 	return len;
 }
@@ -154,24 +157,31 @@ ssize_t baza_write(struct file *pfile, const char __user *buffer, size_t length,
 
 	if (buff[0]=='i' && buff[1]=='z' && buff[2]=='b' && buff[3]=='r' && buff[4]=='i' && buff[5]=='s' && buff[6]=='i')
 	{
+		//zastita deljenih resursa i blokiranje
+		if(down_interruptible(&sem))
+			return -ERESTARTSYS;
+		while(pos == 0)
+		{
+			up(&sem);
+			if(wait_event_interruptible(brisanjeQ,(pos>0)))
+				return -ERESTARTSYS;
+			if(down_interruptible(&sem))
+				return -ERESTARTSYS;
+		}
+
+		//brisanje studenta
 		if(pos>0)
 		{
 			ret = sscanf(buff,"%20[^=]=%100[^,],%100[^,],%s", rec, tmp_ime, tmp_prezime, tmp_brIndexa);
-			if(ret==4)//four parameters parsed in sscanf
+			if(ret==4)
 			{
-				printk(KERN_INFO "Brisanje: "); 
-				printk(KERN_INFO "ime %s ", tmp_ime); 
-				printk(KERN_INFO "prezime %s ", tmp_prezime); 
-				printk(KERN_INFO "broj indexa %s ", tmp_brIndexa); 
-				
 				br=0;	
 				for (i=0; i<pos; i++)
 				{
-					printk(KERN_INFO "Ime u memoriji %s \n",ime[i]);
 					if(strcmp(ime[i],tmp_ime)==0 &&  strcmp(prezime[i],tmp_prezime)==0 && strcmp(brIndexa[i],tmp_brIndexa)==0 )
 					{
-						br++;
-						for(j=i; j<pos-1; j++)
+						br++; // takav student je pronadjen
+						for(j=i; j<pos-1; j++) // brisanje studenta i pomeranje cele baze za mesto udesno 
 						{
 							strcpy(ime[j],ime[j+1]);
 							strcpy(prezime[j],prezime[j+1]);
@@ -184,7 +194,6 @@ ssize_t baza_write(struct file *pfile, const char __user *buffer, size_t length,
 						brBodova[pos-1] = 0;
 						pos=pos-1;
 					    	printk(KERN_INFO "Uspesno izbrisan zeljeni student\n");
-				
 						break; 
 					}
 				}
@@ -192,62 +201,72 @@ ssize_t baza_write(struct file *pfile, const char __user *buffer, size_t length,
 					printk(KERN_WARNING "Nema takvog studenta u bazi\n");
 			}
 			else
-			{
 				printk(KERN_WARNING "Komanda mora biti izbrisi=ime,prezime,brIndexa\n");
-			}
 		}
 		else
-		{	
 			printk(KERN_WARNING "Baza je prazna, ne mozes izbrisati podatak\n");
-		}
+
+		//drugi deo zastite resursa i blokiranja
+		up(&sem);
+		wake_up_interruptible(&upisQ);
+
 	}
 	else
 	{
+		//zastita deljenih resursa i blokiranje
+		if(down_interruptible(&sem))
+			return -ERESTARTSYS;
+		while(pos == BAZA_SIZE)
+		{
+			up(&sem);
+			if(wait_event_interruptible(upisQ,(pos<BAZA_SIZE)))
+				return -ERESTARTSYS;
+			if(down_interruptible(&sem))
+				return -ERESTARTSYS;
+		}
+
+		// upis tj. izmena br. bodova
 		if(pos<BAZA_SIZE)
 		{
 			ret = sscanf(buff,"%100[^,],%100[^,],%100[^=]=%d", tmp_ime, tmp_prezime, tmp_brIndexa, &tmp_brBodova);
-			if(ret==4)//four parameters parsed in sscanf
+			if(ret==4)
 			{	
 				br=0;
 				for (i=0; i<pos; i++)
 				{
 					if(strcmp(ime[i],tmp_ime)==0 &&  strcmp(prezime[i],tmp_prezime)==0 && strcmp(brIndexa[i],tmp_brIndexa)==0 )
 					{
-						br++;
-						if(brBodova[i]==tmp_brBodova)
+						br++; //takav student je pronadjen
+						if(brBodova[i]==tmp_brBodova) 
 				    			printk(KERN_INFO "Takav student vec postoji u bazi\n");
 						else
 						{
-							brBodova[i]=tmp_brBodova;					
+							brBodova[i]=tmp_brBodova; //izmena broja bodova studentu					
 				    			printk(KERN_INFO "Izmena broja bodova studentu\n");
 						}
 						break;
 					}
 				}
-				if(br==0)
+				if(br==0) //upis studenta
 				{	
-					printk(KERN_INFO "UPIS:");
 				       	strcpy(ime[pos],tmp_ime);
 				       	strcpy(prezime[pos],tmp_prezime);
 				       	strcpy(brIndexa[pos],tmp_brIndexa);
 				       	brBodova[pos]=tmp_brBodova;
-					printk(KERN_INFO "ime: %s ", ime[pos]); 
-					printk(KERN_INFO "prezime: %s ", prezime[pos]); 
-					printk(KERN_INFO "broj indexa: %s ", brIndexa[pos]); 
-					printk(KERN_INFO "broj bodova: %d ", brBodova[pos]); 
-					printk(KERN_INFO "Pozicija na koju se upisuje: %d ", pos); 
+					printk(KERN_INFO "Uspesno upisan student na poziciji %d ", pos); 
 					pos=pos+1;
 				}
 			}
 			else
-			{
 				printk(KERN_WARNING "Komanda mora biti ime_prezime_brIndexa=brBodova\n");
-			}
 		}
 		else
-		{	
 			printk(KERN_WARNING "Baza je puna, podatak se ne moze upisati\n");
-		}
+	
+		//drugi deo zastite deljenih resursa i blokiranja
+		up(&sem);
+		wake_up_interruptible(&brisanjeQ);
+	
 	}
 
 	return length;
@@ -258,7 +277,7 @@ static int __init baza_init(void)
    int ret = 0;
 	int i=0;
 	
-//	sema_init(&sem,1);
+	sema_init(&sem,1);
 
 	//Initialize array
 	for (i=0; i<BAZA_SIZE; i++)

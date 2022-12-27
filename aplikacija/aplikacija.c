2b6e52d829d44f482fd4e95cd2f21e74c24ef775
Kosana66 <kosanapavlovic66@gmail.com>
2022-12-27 15:01:42 +0100

    Uspesno implementiranje upisa, citanja, izmene i izlaza

--- aplikacija/aplikacija.c
+++ aplikacija/aplikacija.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define BAZA_SIZE 10
 #define MAX_SIZE 20
@@ -14,66 +15,10 @@ char prezime[BAZA_SIZE][MAX_SIZE];
 char brIndexa[BAZA_SIZE][MAX_SIZE];
 int brBodova[BAZA_SIZE];
 
-void pokupiStudenta(int pos)
-{
-	printf("Unesite ime: \t");
-	scanf("%s",ime[pos]);
-	printf("\nUnesite prezime: \t");
-	scanf("%s",prezime[pos]);
-	printf("\nUnesite broj indeksa: \t");
-	scanf("%s",brIndexa[pos]);
-	printf("\nUnesite broj bodova: \t");
-	scanf("%d",&brBodova[pos]);
-	printf("\n");
-}
-
-int unos(int *pos)
-{
-		fp = fopen ("/dev/baza", "a");
-		if(fp==NULL)
-		{
-			puts("Problem pri otvaranju /dev/baza \n");
-			return -1;
-		}
-		
-		pokupiStudenta(*pos);
-		fprintf(fp,"%s,%s,%s=%d\n", ime[*pos], prezime[*pos], brIndexa[*pos], brBodova[*pos]);
-		printf("ime: %s", ime[*pos]);
-			printf("prezime: %s",prezime[*pos]);
-	
-		(*pos)++;
-
-		if(fclose(fp))
-		{
-			puts("Problem pri zatvaranju /dev/switch");
-			return -1;
-		}
-		printf("Uspesan unos!\n");
-}		
-
-int citanje(int *pos)
-{
-		fp = fopen ("/dev/baza", "r");
-		if(fp==NULL)
-		{
-			puts("Problem pri otvaranju /dev/baza \n");
-			return -1;
-		}
-		
-		for(int i=0; i<(*pos); i++)
-		{
-			fscanf(fp,"%s,%s,%s=%d\n",ime[i], prezime[i], brIndexa[i], &brBodova[i]);
-			printf("%s %s %s - %d\n", brIndexa[i], ime[i], prezime[i], brBodova[i]);
-		}
-
-		if(fclose(fp))
-		{
-			puts("Problem pri zatvaranju /dev/switch");
-			return -1;
-		}
-		printf("Uspesno citanje!\n");
-}
-
+char tmp_ime[MAX_SIZE];
+char tmp_prezime[MAX_SIZE];
+char tmp_brIndexa[MAX_SIZE];
+int tmp_brBodova;
 
 int main ()
 {
@@ -84,7 +29,7 @@ int main ()
 		int opcija=0;
 		do
 		{
-			printf("IZABERITE OPCIJU: \n");
+			printf("\nIZABERITE OPCIJU: \n");
 			printf("1. Unos novog studenta \n");
 			printf("2. Izmena broja bodova studenta \n");
 			printf("3. Brisanje studenta \n");
@@ -97,10 +42,100 @@ int main ()
 
 		switch(opcija)
 		{
-			case 1: unos(&pos); 
+			case 1: 
+
+				printf("Unesite ime: ");
+				scanf("%s",ime[pos]);
+				printf("\nUnesite prezime: ");
+				scanf("%s",prezime[pos]);
+				printf("\nUnesite broj indeksa: ");
+				scanf("%s",brIndexa[pos]);
+				printf("\nUnesite broj bodova: ");
+				scanf("%d",&brBodova[pos]);
+				printf("\n");
+		
+				
+				fp = fopen ("/dev/baza", "w");
+				if(fp==NULL)
+				{
+					puts("Problem pri otvaranju /dev/baza \n");
+					return -1;
+				}
+		
+				fprintf(fp,"%s,%s,%s=%d\n", ime[pos], prezime[pos], brIndexa[pos], brBodova[pos]);
+				pos++;
+
+				if(fclose(fp))
+				{
+					puts("Problem pri zatvaranju /dev/baza\n");
+					return -1;
+				}
+				printf("\nUspesan unos!\n");
+		
+			break;
+				
+			case 2: 
+					
+				printf("Unesite ime: ");
+				scanf("%s",tmp_ime);
+				printf("\nUnesite prezime: ");
+				scanf("%s",tmp_prezime);
+				printf("\nUnesite broj indeksa: ");
+				scanf("%s",tmp_brIndexa);
+				printf("\nUnesite novi broj bodova: ");
+				scanf("%d", &tmp_brBodova);
+				printf("\n");
+		
+				
+				fp = fopen ("/dev/baza", "a+");
+				if(fp==NULL)
+				{
+					puts("Problem pri otvaranju /dev/baza \n");
+					return -1;
+				}
+					
+				for(int i=0; i<BAZA_SIZE; i++)
+				{
+					while(fscanf(fp,"%s %s %s - %d\n", brIndexa[i], ime[i], prezime[i], &brBodova[i]) != EOF)
+					{
+						if(strcmp(ime[i],tmp_ime)==0 && strcmp(prezime[i],tmp_prezime)==0 && strcmp(brIndexa[i],tmp_brIndexa)==0 )
+						{
+							brBodova[i]=tmp_brBodova;
+							fprintf(fp,"%s,%s,%s=%d\n", ime[i], prezime[i], brIndexa[i], brBodova[i]);
+							printf("Uspesna izmena! \n");
+						}
+					}
+				}
+
+				if(fclose(fp))
+				{
+					puts("Problem pri zatvaranju /dev/baza\n");
+					return -1;
+				}
+				
 			break;
 
-			case 4: citanje(&pos); 
+			case 4: 
+				       		
+				fp = fopen ("/dev/baza", "r");
+				if(fp==NULL)
+				{
+					puts("Problem pri otvaranju /dev/baza \n");
+					return -1;
+				}
+				for(int i=0; i<BAZA_SIZE; i++)
+				{
+					while(fscanf(fp,"%s %s %s - %d\n", brIndexa[i], ime[i], prezime[i], &brBodova[i]) != EOF)
+						printf("%s %s %s - %d\n", brIndexa[i], ime[i], prezime[i], brBodova[i]);
+				}
+
+				if(fclose(fp))
+				{
+					puts("Problem pri zatvaranju /dev/baza\n");
+					return -1;
+				}
+				printf("\nUspesno citanje!\n");
+
 			break;	
 			
 			case 5: flag=false;
